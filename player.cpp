593ff6446ddfbdbb50523416592ffea3ab672232
DaruaNikko <daruanikko@gmail.com>
2022-03-28 21:48:57 +0300

    added ability to check bulls and cows at Player class

--- player.cpp
+++ player.cpp
@@ -11,3 +11,43 @@ void Player::set_number(QString number) {
 QString Player::get_number() {
     return number;
 }
+
+int Player::get_bulls_for(QString number) {
+    checking_number = number;
+    int num_of_bulls = 0;
+
+    for (int i = 0; i < 4; i++)
+        if (number[i] == checking_number[i])
+            num_of_bulls++;
+
+    return num_of_bulls;
+}
+
+int Player::get_cows_for(QString number) {
+    checking_number = number;
+    int num_of_cows = 0;
+
+    for (int checking_number_i = 0;
+             checking_number_i < 4;
+             checking_number_i++)
+        if (check_cow())
+            num_of_cows++;
+
+    return num_of_cows;
+}
+
+bool Player::check_cow() {
+    for (int number_i = 0; number_i < 4; number_i++)
+        if (!checked_as_bull())
+            if (number[number_i] ==
+                    checking_number[checking_number_i])
+                return true;
+    return false;
+
+}
+
+bool Player::checked_as_bull() {
+    if (number_i == checking_number_i)
+        return true;
+    return false;
+}
--- player.h
+++ player.h
@@ -7,13 +7,20 @@ class Player {
 
 private:
     QString number;
-
+    int number_i;
+    QString checking_number;
+    int checking_number_i;
 
 public:
     Player();
 
     void set_number(QString number);
     QString get_number();
+
+    int get_bulls_for(QString checking_number);
+    int get_cows_for(QString checking_number);
+    bool check_cow();
+    bool checked_as_bull();
 };
 
 #endif // PLAYER_H
